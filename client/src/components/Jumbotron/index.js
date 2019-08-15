74755ce0e046155b74d7b0ef9645e2f6dc0ee3c6
Jeff Ball <jeffball@Jeffs-MacBook-Pro.local>
2019-08-14 20:48:56 -0700

    Add public, src, controllers, models, routes, scripts.

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Jumbotron() {
+    return (
+    <div className='jumbotron jumbotron-fluid bg-info text-white text-center'>
+        <h1 className='display-2'>
+            Google Book Search
+        </h1>
+        <h2 className='lead'>
+            Find and save your favorite books
+        </h2>
+    </div>
+    )
+}
+
+export default Jumbotron;
