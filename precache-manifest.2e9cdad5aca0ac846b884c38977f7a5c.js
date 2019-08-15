f700f4de69669d8ea0d647808c548e80632200e3
Jeff Ball <jeffball@Jeffs-MacBook-Pro.local>
2019-08-14 21:09:19 -0700

    Updates

---
+++ precache-manifest.2e9cdad5aca0ac846b884c38977f7a5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8036014b6c385efeeb7b8badc124c96e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "130c2cce579b22d6817a",
+    "url": "/static/css/main.f19fae40.chunk.css"
+  },
+  {
+    "revision": "9bc46d894fede4fac851",
+    "url": "/static/js/2.95dd7dd5.chunk.js"
+  },
+  {
+    "revision": "130c2cce579b22d6817a",
+    "url": "/static/js/main.ab6ed185.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e325e2a2f1e76142c7bb0ebae1cc548a",
+    "url": "/static/media/library.e325e2a2.jpg"
+  }
+]);
